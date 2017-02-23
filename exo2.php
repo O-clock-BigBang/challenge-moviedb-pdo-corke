f9deaa64c45e25cfc472c19a32750301cb1261d0
corke <corke.aj@gmail.com>
2017-02-23 18:29:09 +0100

    exo 2

--- exo2.php
+++ exo2.php
@@ -7,7 +7,19 @@ require('dbconnect.php');
 - éxécutez là (avec PDO)
 - affichez le résultat sous forme d'une liste html
 */
-
+$sql = "select * from genres;";
+$query=$db->query($sql);
+$genres = $query->fetchAll();
+foreach($genres as $un_genre){
+    echo '<li>' . $un_genre['name'] . '</li>';
+}
 /*
 Reprenez cette requête, mais affichez aussi le nombre de films associés à chaque genre
 */
+
+$sql = "SELECT name ,count(movies.id) as nb_films FROM `genres`, `movie_genre`, `movies` WHERE genres.id=movie_genre.genre_id and movie_genre.movie_id=movies.id GROUP BY genres.id;";
+$query=$db->query($sql);
+$genres = $query->fetchAll();
+foreach($genres as $un_genre){
+    echo '<li>' . $un_genre['name'] . ' - ' . $un_genre['nb_films'] . '</li>';
+}
